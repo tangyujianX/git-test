544fb02455ad2b409f6f93ab22b05b233faceca3
tyj <tyj@my.com>
2023-01-07 23:05:18 +0800

    hot-fix commit2

--- venv/main/guinate/git/GitTest.py
+++ venv/main/guinate/git/GitTest.py
@@ -1,10 +1,10 @@
-
 class GitTest:
     def p():
         print("hello git!")
         print("hello world!")
         print("hello ketty")
         print("good morning")
+        print("hot-fix test!")
 
 
 obj = GitTest
